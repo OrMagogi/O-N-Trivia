8f82158c6c79712ddb4a7991cae404c72c2b67f7
naor555666 <naor555666@gmail.com>
2021-02-03 22:08:08 +0200

    login and register working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { TriviaService } from './trivia.service';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { FormGroup } from '@angular/forms';
 import { User } from './user'
-import {Routes,RouterModule } from '@angular/router';
-import {MatDialog} from '@angular/material/dialog';
+import { Routes, RouterModule } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 import { LogoutDialogComponent } from './logout-dialog/logout-dialog.component';
+import { UseExistingWebDriver } from 'protractor/built/driverProviders';
 
+//response coded
+const NO_USER_FOUND = 1
+const USER_ALREADY_REGISTERED = 2
+const SIGNED_UP_SUCCESSFULY = 3
 
 @Component({
   selector: 'app-root',
@@ -15,27 +20,30 @@ import { LogoutDialogComponent } from './logout-dialog/logout-dialog.component';
 })
 export class AppComponent {
   title = 'my-app1';
-  public userName= "Or";
-  public counter=0;
+  public userName = "Or";
+  public counter = 0;
   public usernameColor;
   public isLoggedIn;
   public user;
-  public modalTitle:string;
-  public pageToDisplay="homepage_template";
+  public modalTitle: string;
+  public pageToDisplay = "homepage_template";
   //public questionsList =[];
   closeResult = '';
+  constructor(private _triviaService: TriviaService, private modalService: NgbModal, private dialog: MatDialog) { }
 
-  constructor(private _triviaService : TriviaService, private modalService: NgbModal,private dialog: MatDialog){}
-
-  ngOnInit(){
-    this.user = new User("","",0);
-    this.isLoggedIn=false;
+  ngOnInit() {
+    this.user = new User("", "", 0);
+    console.log("is user logged?: " + localStorage["loggedUser"]);
+    this.isLoggedIn = localStorage["loggedUser"] != "undefined"
+    // if (this.isLoggedIn) {
+    //   this.user = new User(localStorage["loggedUser"].userName, "", localStorage["loggedUser"].maxScore);
+    // }
   }
 
 
-  open(content,action:string) {
+  open(content, action: string) {
     this.modalTitle = action;
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
@@ -52,28 +60,62 @@ export class AppComponent {
     }
   }
 
-  onSubmit(operation:string){
-    this.user.highScore =0;
-    //alert(operation+": "+this.user.userName +" "+ this.user.password +" "+ this.user.highScore);
-    if(operation=='Log in'){   // to be changed
-      // this.isLoggedIn=true;
-      
+  onSubmit(operation: string) {
+    this.user.highScore = 0;
+    if (operation == 'Log in') {   // to be changed
+      this.handleLogin()
+    } else if (operation == 'Registration') {
+      this.handleRegistration()
     }
   }
 
-  setPage(templateName){
+  handleLogin() {
+    this._triviaService.getUserByUserNameAndPassword(this.user.userName, this.user.password).subscribe((data: any) => {
+      if (data?.errorCode) {
+        console.log("Problem occured");
+      } else {
+        if (data?.userName) {
+          localStorage["loggedUser"] = data
+          this.user.userName = data.userName
+          this.isLoggedIn = true
+        } else {
+          console.log("No user found");
+        }
+      }
+      console.log(data)
+    });
+  }
+
+  handleRegistration() {
+    this._triviaService.signUpUser(this.user.userName, this.user.password).subscribe((data: any) => {
+      if (data?.errorCode) {
+        if (data.errorCode == USER_ALREADY_REGISTERED) {
+          console.log("Username already taken, try another");
+        } else {
+          console.log('Problem signing up');
+        }
+      } else {
+        console.log('Signed up succssefuly');
+      }
+      console.log(data)
+    });
+  }
+
+  setPage(templateName) {
     this.pageToDisplay = templateName;
 
   }
 
-  openLogoutDialog(){
-    let dialogRef= this.dialog.open(LogoutDialogComponent);
-    dialogRef.afterClosed().subscribe(result=>{
-      if(result){
-        this.isLoggedIn=false;
+  openLogoutDialog() {
+    let dialogRef = this.dialog.open(LogoutDialogComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      if (result == "logout") {
+        console.log(result);
+        localStorage["loggedUser"] = undefined
+        this.isLoggedIn = false
       }
     })
   }
 
-  
 }
+
--- src/app/trivia.service.ts
+++ src/app/trivia.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IQuestion } from './IQuestion';
 import { INews } from './INews';
@@ -10,21 +10,36 @@ import { INews } from './INews';
 })
 export class TriviaService {
 
-  private _questionsUrl : string = "/assets/data/questions.json";
-  private _newsUrl : string = "/assets/data/news.json";
-  private _infoUrl : string = "/assets/data/information.json";
+  // private _questionsUrl : string = "/assets/data/questions.json";
+  private _questionsUrl: string = "http://localhost:5000/getQuestions";
+  private _getUserUrl: string = "http://localhost:5000/getUserByUserNameAndPassword";
+  private _signUpUserUrl: string = "http://localhost:5000/signUpUser";
+  private _newsUrl: string = "/assets/data/news.json";
+  private _infoUrl: string = "/assets/data/information.json";
 
   constructor(private http: HttpClient) { }
 
-  getQuestions(): Observable<IQuestion[]>{
-    return this.http.get<IQuestion[]>(this._questionsUrl);
+  getQuestions(): Observable<string> {
+    return this.http.get<string>(this._questionsUrl);
   }
 
-  getNews(): Observable<INews[]>{
+
+
+  getUserByUserNameAndPassword(_userName: string, _password: string): Observable<string> {
+    let user = { userName: _userName, password: _password }    
+    return this.http.post<string>(this._getUserUrl, user);
+  }
+
+  signUpUser(_userName: string, _password: string): Observable<string> {
+    let user = { userName: _userName, password: _password }    
+    return this.http.post<string>(this._signUpUserUrl, user);
+  }
+
+  getNews(): Observable<INews[]> {
     return this.http.get<INews[]>(this._newsUrl);
   }
 
-  getInformation(): Observable<INews[]>{
+  getInformation(): Observable<INews[]> {
     return this.http.get<INews[]>(this._infoUrl);
   }
 }
